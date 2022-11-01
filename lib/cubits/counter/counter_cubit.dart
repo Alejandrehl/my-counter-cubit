418c3bed43d8f34b1555c2302af5dae207b231ec
Alejandro Hernández <alejandrehl@icloud.com>
2022-11-01 11:22:29 -0300

    Seteando estado con factory contructor

--- lib/cubits/counter/counter_cubit.dart
+++ lib/cubits/counter/counter_cubit.dart
@@ -1,9 +1,8 @@
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  CounterCubit() : super(const CounterInitial(counter: 0));
+  CounterCubit() : super(CounterState.initial());
 }
--- lib/cubits/counter/counter_state.dart
+++ lib/cubits/counter/counter_state.dart
@@ -1,23 +1,27 @@
 part of 'counter_cubit.dart';
 
-@immutable
-abstract class CounterState extends Equatable {
-  const CounterState();
-
-  @override
-  List<Object?> get props => [];
-}
-
-class CounterInitial extends CounterState {
+class CounterState extends Equatable {
   final int counter;
 
-  const CounterInitial({
+  const CounterState({
     required this.counter,
   });
+
+  factory CounterState.initial() {
+    return const CounterState(counter: 0);
+  }
 
   @override
   List<Object> get props => [counter];
 
   @override
   String toString() => 'CounterState(counter: $counter)';
+
+  CounterState copyWith({
+    int? counter,
+  }) {
+    return CounterState(
+      counter: counter ?? this.counter,
+    );
+  }
 }
