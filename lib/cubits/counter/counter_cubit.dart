3318946cd7a34b4530c8cb1da7a8dd89ebd01993
Alejandro Hernández <alejandrehl@icloud.com>
2022-11-01 11:29:15 -0300

    Increment and credement method in counter cubit

--- lib/cubits/counter/counter_cubit.dart
+++ lib/cubits/counter/counter_cubit.dart
@@ -5,4 +5,12 @@ part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit() : super(CounterState.initial());
+
+  void increment() {
+    emit(state.copyWith(counter: state.counter + 1));
+  }
+
+  void decrement() {
+    emit(state.copyWith(counter: state.counter - 1));
+  }
 }
